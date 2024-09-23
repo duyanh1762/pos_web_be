bc9ea10daa598b3cbd879f910c64734884fa7c99
duyanh1762 <deadpool692.0@gmail.com>
2024-09-23 20:58:28 +0700

    add port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: ['http://localhost:4200',"http://localhost:56000"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
