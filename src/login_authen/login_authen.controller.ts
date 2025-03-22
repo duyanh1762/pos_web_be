619e4a5145d71ab3a4c7725da15196c14ba734c6
duyanh1762 <deadpool692.0@gmail.com>
2025-03-22 20:45:04 +0700

    add login test for config

--- src/login_authen/login_authen.controller.ts
+++ src/login_authen/login_authen.controller.ts
@@ -6,27 +6,61 @@ import { StaffEntity } from 'src/staff/staff.entity/staff.entity';
 import { UserEntity } from 'src/user/user.entity/user.entity';
 import { Repository } from 'typeorm';
 
-interface DataReQuest{
+// adminUser:StaffEntity = {
+//     id:0,
+//     shopID:0,
+//     name:"....",
+//     password:"....",
+//     role:"admin",
+//     status:"Active"
+// }
+
+interface ShopReQuest{
     username: string,
     password:string,
     authType: string,
+}
+interface StaffReQuest{
+    username: string,
+    password:string,
+    authType: string,
+    shopID:number,
 }
 interface UserLogin{
     phoneName:string,
     password:string,
 }
+interface ConFigLogin{
+    username:string,
+    password:string,
+}
+
 @Controller('login-authen')
 export class LoginAuthenController {
+
     constructor(
         @InjectRepository(ShopEntity) private readonly shopRepo:Repository<ShopEntity>,
         @InjectRepository(StaffEntity) private readonly staffRepo:Repository<StaffEntity>,
         @InjectRepository(UserEntity) private readonly userRepo:Repository<UserEntity>,
-    ){}
+    ){
+        let admin:any = {
+            username:"admin",
+            password:"admin666",
+            role:"admin"
+        };
+        
+        let warehouse:any = {
+            username:"kho",
+            password:"warehouse666",
+            role:"warehouse"
+        }
+     }
 
     @Post()
-    async authenLogin(@Body() data:DataReQuest){
+    async authenLogin(@Body() data:ShopReQuest | StaffReQuest | any){
         let checked;
         let shopInfor:ShopEntity;
+        let staffInfor:StaffEntity;
         if(data.authType === "SHOP_AUTH"){
             let shops =  this.shopRepo.find();
             await shops.then((rs)=>{
@@ -38,27 +72,54 @@ export class LoginAuthenController {
                 });
             });
         }else if(data.authType === "STAFF_AUTH"){
+            let request:StaffReQuest = data;
             let staffs = this.staffRepo.find();
             await staffs.then((rs)=>{
                 rs.forEach((staff)=>{
-                    if(data.username === staff.name && data.password === staff.password){
+                    if(request.username === staff.name && request.password === staff.password && request.shopID === staff.shopID){
                         checked = true;
+                        staffInfor = staff;
                     }
                 });
             });
         }else{
             return false;
         }
-        if(checked === true){
+        if(checked === true && data.authType === "SHOP_AUTH"){
             return {
                 success:true,
                 data:shopInfor,
             };
+        }else if(checked === true && data.authType === "STAFF_AUTH"){
+            return {
+                success:true,
+                data:staffInfor,
+            }
         }else{
             return {
                 success:false,
                 data:null,
             };
+        }
+    }
+
+    @Post("config")
+    async authenLoginForConfig(@Body() data:ConFigLogin){
+        if(data.username === "admin" && data.password === "admin666"){
+            return {
+                success:true,
+                role:"admin",
+            }
+        }else if(data.username === "kho" && data.password === "warehouse666"){
+            return {
+                success:true,
+                role:"warehouse",
+            }
+        }else{
+            return {
+                success:false,
+                role:"",
+            }
         }
     }
 
